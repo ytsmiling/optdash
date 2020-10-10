57453af71161a5f806553062417b84639c61ffbf
ytsmiling <ytsmiling@gmail.com>
2020-10-11 00:10:44 +0900

    Add frontend files to package-data.

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
     install_requires=get_install_requires(),
     tests_require=get_tests_require(),
     extras_require=get_extras_require(),
+    package_data={"optdash": ["*/public/*"]},
     entry_points={
         "console_scripts": [
             "optdash = optdash.server:main",
