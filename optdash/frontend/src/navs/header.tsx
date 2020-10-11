da521f5e9fb5c6e6ebd43334a52cb6c71b6f9d12
ytsmiling <ytsmiling@gmail.com>
2020-10-11 16:30:12 +0900

    Add github-issue button.

--- optdash/frontend/src/navs/header.tsx
+++ optdash/frontend/src/navs/header.tsx
@@ -6,10 +6,15 @@ function Header() {
     return (
         <header className="header">
             <div className="header-content">
-                <span className="header-title">
+                <span className="header-inline-block">
                     Optuna Dashboard
                 </span>
-      <GitHubButton href="https://github.com/ytsmiling/optdash" data-size="large" data-icon="octicon-star" aria-label="Star ytsmiling/optdash on GitHub">Star</GitHubButton>
+                <span className="header-inline-block">
+                    <GitHubButton href="https://github.com/ytsmiling/optdash" data-size="large" data-icon="octicon-star" aria-label="Star ytsmiling/optdash on GitHub">Star</GitHubButton>
+                </span>
+                <span className="header-inline-block">
+                    <GitHubButton href="https://github.com/ytsmiling/optdash/issues" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-size="large" aria-label="Issue ytsmiling/optdash on GitHub">Issue</GitHubButton>
+                </span>
             </div>
         </header>
     );
